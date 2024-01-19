8a216498ed30554cf15dc1e2b1f2d621d3bb86b2
helloworld <36910384@qq.com>
2024-01-19 22:29:26 +0800

    p25

---
+++ SeqQueue/main.c
@@ -0,0 +1,23 @@
+#include"seq_queue.h"
+int main()
+{
+	seq_queue q;
+	init_queue(&q);
+	for(int i=30;i>0;i--)
+	{
+		en_queue(&q,i);
+	}
+	show_queue(&q);
+	int val=0;
+	for(int i=11;i>0;i--)
+	{
+
+		get_head(&q,&val);
+		de_queue(&q);
+		show_queue(&q);
+	}
+	clear_queue(&q);
+	show_queue(&q);
+	
+	return 0;
+}
